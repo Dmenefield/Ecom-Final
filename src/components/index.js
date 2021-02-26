085fe827b1a7c6d9fe5914e17208c7af6c59bece
Dmenefield <dmenefieldjr@gmail.com>
2021-02-25 23:17:08 -0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from './navbar/navbar';
+export { default as Products } from './products/products';
+export { default as Cart } from './cart/cart';
+export { default as Checkout } from '../components/Checkout-form/Checkout/checkout'
+export { default as AddressForm } from './Checkout-form/addressform'
